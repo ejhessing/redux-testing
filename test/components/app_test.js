c7cbdad18723198fa490c43aeb790c611685ba65
Erwin Hessing <ejhessing@Erwins-MacBook-Pro.local>
2016-09-22 22:26:19 +1200

    Testing Boiler Plate

---
+++ test/components/app_test.js
@@ -0,0 +1,13 @@
+import { renderComponent, expect } from '../test_helper'
+import App from '../../src/components/app'
+
+
+// Use 'describe' to group together similar tests
+describe('App Component')
+
+
+// Use 'it' to test a single attrivute of a target
+it ('Shows the correct text')
+
+// use 'expect' to make an 'assertion' about a target
+expect()
